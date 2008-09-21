f8de25546b66100a62556de7bb733d35012ff341
Michael Fellinger <m.fellinger@gmail.com>
2008-09-21 23:08:03 +0900

    Add example for Helper::Link

---
+++ example/link.rb
@@ -0,0 +1,36 @@
+require 'rubygems'
+require 'innate'
+
+class Linking
+  include Innate::Node
+  map '/'
+
+  def index
+    "simple link<br />" +
+      a('Help?', :help)
+  end
+
+  def new
+    "Something new!"
+  end
+
+  def help
+    "You have help<br />" +
+      Different.a('A Different Node', :another)
+  end
+end
+
+class Different
+  include Innate::Node
+  map '/link_to'
+
+  def another
+    a('Even deeper', 'and/deeper')
+  end
+
+  def and__deeper
+    Linking.a('Back to Linking Node', :index)
+  end
+end
+
+Innate.start :adapter => :mongrel          
