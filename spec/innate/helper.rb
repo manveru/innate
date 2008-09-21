e6f86ba27a662824efc08019f0d110b9dce85f74
Michael Fellinger <m.fellinger@gmail.com>
2008-09-21 23:07:36 +0900

    Mods to spec for Helper::Link

--- spec/innate/helper.rb
+++ spec/innate/helper.rb
@@ -11,8 +11,8 @@ module Innate
       end
     end
 
-    module NoSmileHelper
-      def no_smile
+    module FrownHelper
+      def frown
         ':('
       end
     end
@@ -23,13 +23,13 @@ class HelperNodeExpose
   include Innate::Node
   map '/'
 
-  helper :smile_helper, :no_smile_helper
+  helper :smile_helper, :frown_helper
 end
 
 describe HelperNodeExpose do
   should 'expose an action' do
     Innate::Mock.get('/smile').body.should == ':)'
-    Innate::Mock.get('/no_smile').status.should == 404
+    Innate::Mock.get('/frown').status.should == 404
   end
 end
 
@@ -42,6 +42,7 @@ end
 
 describe Innate::Helper::Link do
   FNL = FooNodeLink
+
   should 'construct URI from ::r' do
     FNL.r(:index).should == URI('/foo/index')
     FNL.r(:/).should == URI('/foo/')
@@ -52,9 +53,9 @@ describe Innate::Helper::Link do
   end
 
   should 'construct link from ::a' do
-    FNL.a(:index).should == "<a href='/foo/index'>index</a>"
-    FNL.a(:index, :x => :y).should == "<a href='/foo/index?x=y'>index</a>"
-    FNL.a('duh/bar', :x => :y).should == "<a href='/foo/duh/bar?x=y'>duh/bar</a>"
-    FNL.a('home', :href => FNL.r(:home)).should == "<a href='/foo/home'>home</a>"
+    FNL.a(:index).should == '<a href="/foo/index">index</a>'
+    FNL.a('index', :index, :x => :y).should == '<a href="/foo/index?x=y">index</a>'
+    FNL.a('duh/bar', 'duh/bar', :x => :y).should == '<a href="/foo/duh/bar?x=y">duh/bar</a>'
+    FNL.a('foo', :/, :x => :y).should == '<a href="/foo/?x=y">foo</a>'
   end
 end
