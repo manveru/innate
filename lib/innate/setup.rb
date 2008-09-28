76fcd85109f565f82dc21336e9aec7c856ee97f4
Michael Fellinger <m.fellinger@gmail.com>
2008-09-28 18:34:50 +0900

    Adding innate/setup.rb as stub for the ramaze one

---
+++ lib/innate/setup.rb
@@ -0,0 +1,24 @@
+module Innate
+  def self.setup(&block)
+    Setup.new(&block)
+  end
+
+  class Setup
+    def initialize(&block)
+      instance_eval(&block) if block
+    end
+
+    def gem(*args)
+      raise
+    end
+
+    def start(options = {})
+      options[:caller] ||= caller
+      Innate.start(options)
+    end
+
+    def require(*libs)
+      libs.flatten.each{|lib| Kernel::require(lib) }
+    end
+  end
+end
