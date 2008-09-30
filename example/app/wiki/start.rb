47f4112879406e887cc970c74ba934292749ce80
Michael Fellinger <m.fellinger@gmail.com>
2008-09-30 16:10:37 +0900

    custom middleware setup for wiki

--- example/app/wiki/start.rb
+++ example/app/wiki/start.rb
@@ -4,6 +4,18 @@ require 'owlscribble'
 require 'uv'
 require 'git'
 
+Innate.middleware :innate do |m|
+  m.use Rack::CommonLogger # usually fast, depending on the output
+  m.use Rack::ShowExceptions # fast
+  m.use Rack::ShowStatus     # fast
+  m.use Rack::Reloader       # reasonably fast depending on settings
+  m.use Rack::Lint         # slow, use only while developing
+  # m.use Rack::Profile      # slow, use only for debugging or tuning
+  m.use Innate::Current      # necessary
+
+  m.cascade Rack::File.new('public'), Innate::DynaMap
+end
+
 Innate.setup do
 #   gem :owlscribble
 #   gem :uv
