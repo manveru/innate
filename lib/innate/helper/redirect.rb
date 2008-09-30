0d5138edb0cf0b07ebc24c8958beefdb3c49dde1
Michael Fellinger <m.fellinger@gmail.com>
2008-09-30 22:15:16 +0900

    Refactor of Redirect helper

--- lib/innate/helper/redirect.rb
+++ lib/innate/helper/redirect.rb
@@ -12,17 +12,39 @@ module Innate
       end
 
       def respond!(body, status, header)
+        throw(:respond, Response.new(body, status, header))
       end
 
+      # +target+ should be anything responding to #to_s.
+      # To check or modify the URI the redirect will go to you may pass a
+      # block, the result value of the block is ignored:
+      #
+      #   redirect("/"){|uri| uri.scheme = 'http' }
+      #   redirect("/"){|uri| uri.host = 'secure.com' if uri.scheme =~ /s/ }
+      #
+      # +options+ may contain:
+      #
+      #   :scheme => "http" | "https" | "ftp" | ...
+      #   :host   => "localhost" | "foo.com" | "123.123.123.123" | ...
+      #   :port   => 7000 | "80" | 80 | ...
+      #
+      #   :status => 302 | 300 | 303 | ...
+      #   :body   => "This is a redirect, hold on while we teleport" | ...
+      #
+      #   :raw!   => true | false | nil | ...
+      #
+      # Note that all options are optional and you may just pass a +target+.
+
       def redirect(target, options = {})
-        uri = URI(target)
-        uri.scheme = request.scheme unless uri.scheme
-        uri.host   = request.host unless uri.host
+        uri = URI(target.to_s)
+        uri.scheme ||= options[:scheme] || request.scheme
+        uri.host   ||= options[:host]   || request.host
+        uri.port   ||= options[:port]   || request.port
         uri = URI(uri.to_s)
 
         yield(uri) if block_given?
 
-        raw_redirect(uri, options)
+        options[:raw!] ? raw_redirect!(uri, options) : raw_redirect(uri, options)
       end
 
       def raw_redirect(target, options = {})
@@ -32,7 +54,19 @@ module Innate
         response.status = options[:status] || 302
         response.write    options[:body]   || redirect_body(target)
 
-        throw(:redirect)
+        Log.debug "Redirect to: #{target}"
+        throw(:redirect, response)
+      end
+
+      # Don't reuse the current response object
+
+      def raw_redirect!(target, options = {}, &block)
+        header = {'Location' => target}
+        status = options[:status] || 302
+        body   = options[:body] || redirect_body(target)
+
+        Log.debug "Redirect to: #{target}"
+        throw(:redirect, Response.new(body, status, header, &block))
       end
 
       def redirect_body(target)
@@ -43,6 +77,7 @@ module Innate
       def redirect_referrer
         redirect request.referer
       end
+      alias redirect_referer redirect_referrer
     end
   end
 end
