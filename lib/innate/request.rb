5090bee48c6ee693296e0fc74356f9b0c0b4bc7d
Michael Fellinger <m.fellinger@gmail.com>
2008-11-16 20:42:43 +0900

    Some basic methods for Request

--- lib/innate/request.rb
+++ lib/innate/request.rb
@@ -1,8 +1,32 @@
 module Innate
+  # Subclass Rack::Request and add some convenient methods.
   class Request < Rack::Request
+    def self.current
+      Current.request
+    end
+
     def [](value, *keys)
       return params[value.to_s] if keys.empty?
       [value, *keys].map{|k| params[k.to_s] }
+    end
+
+    def request_uri
+      env['REQUEST_URI'] || env['PATH_INFO']
+    end
+
+    def subset(*keys)
+      keys = keys.map{|k| k.to_s }
+      params.reject{|k,v| not keys.include?(k) }
+    end
+
+    def domain(path = '/')
+      scheme = env['rack.url_scheme'] || 'http'
+      host = env['HTTP_HOST']
+      URI("#{scheme}://#{host}#{path}")
+    end
+
+    def locales
+      env['HTTP_ACCEPT_LANGUAGE'].to_s.split(/(?:,|;q=[\d.,]+)/)
     end
   end
 end
