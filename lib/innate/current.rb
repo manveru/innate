816dad67505be2902cacc5067843d6c38855fde9
Michael Fellinger <m.fellinger@gmail.com>
2008-12-26 23:41:06 +0900

    Document Innate::Current

--- lib/innate/current.rb
+++ lib/innate/current.rb
@@ -2,6 +2,9 @@ require 'innate/request'
 # require 'innate/response'
 
 module Innate
+  # Uses STATE to scope request/response/session per Fiber/Thread so we can
+  # reach them from anywhere in the code without passing around the objects
+  # directly.
   class Current
     extend Trinity
 
@@ -9,12 +12,16 @@ module Innate
       @app = app
     end
 
+    # Wrap into STATE, run setup and call the app inside STATE.
+
     def call(env)
       STATE.wrap do
         setup(env)
         @app.call(env)
       end
     end
+
+    # Setup new Request/Response/Session for this request/response cycle
 
     def setup(env)
       req = STATE[:request] = Request.new(env)
