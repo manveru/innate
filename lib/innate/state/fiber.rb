63d2bbd4b58ccb019ece5a585b0f4eaefa15c776
Michael Fellinger <m.fellinger@gmail.com>
2008-12-26 23:46:33 +0900

    Document Innate::State::Thread and Innate::State::Fiber

--- lib/innate/state/fiber.rb
+++ lib/innate/state/fiber.rb
@@ -1,6 +1,19 @@
 require 'fiber'
 
 module Innate
+  # Innate subclasses Fiber to enable lightweight request/respose-cycle local
+  # variables.
+  #
+  # We do that by adding a state Hash to the Fiber instance on initalization
+  # which can be accessed by #[], #[]= and #key?. Other Hash methods are not
+  # necessary right now but may be added.
+  #
+  # We subclass to keep your Ruby clean and polished.
+  #
+  # TODO:
+  #   * 1.9.1 includes a patch from matz that will call #initialize, so we can
+  #     remove our ::new hack, just have to get my hands on a properly
+  #     compiling version of 1.9.1
   class Fiber < ::Fiber
     attr_accessor :state
 
@@ -24,6 +37,9 @@ module Innate
   end
 
   module State
+    # Our accessor to the currently active Fiber, an instance of
+    # Innate::State::Fiber will be assigned to Innate::STATE if fibers are
+    # available.
     class Fiber
       def [](key)
         Innate::Fiber.current[key]
@@ -33,11 +49,18 @@ module Innate
         Innate::Fiber.current[key] = value
       end
 
+      # We don't use Fiber in a concurrent manner and they don't run
+      # concurrently by themselves, so we directly #resume the Fiber to get the
+      # return value of +block+.
+
       def wrap(&block)
         Innate::Fiber.new(&block).resume
       end
 
-      def sync #(&block)
+      # In an environment where only Fiber is used there is no concurrency, so
+      # we don't have to lock with a Mutex.
+
+      def sync
         yield
       end
     end
--- lib/innate/state/thread.rb
+++ lib/innate/state/thread.rb
@@ -2,6 +2,14 @@ require 'thread'
 
 module Innate
   module State
+    # In case fibers are not available we fall back to this wrapper.
+    #
+    # It will raise errors happening inside the wrapping Thread even if
+    # Thread::raise_on_exception is false.
+    #
+    # For things that require a mutex in a threaded environment, use
+    # STATE#sync, if Fiber is available no mutex will be used.
+
     class Thread
       SEMAPHORE = Mutex.new
 
@@ -12,6 +20,10 @@ module Innate
       def []=(key, value)
         ::Thread.current[key] = value
       end
+
+      # Execute given block in a new Thread and rescue any exceptions before
+      # they reach Thread::new, so in case Thread::raise_on_exception is false
+      # we can still reraise the error outside of the Thread.
 
       def wrap
         value = ::Thread.new{ begin; yield; rescue Exception => ex; ex; end }.value
