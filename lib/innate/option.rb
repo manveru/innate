4e58286b960eab59ef2b5b08152ce1a5cd5d536a
Michael Fellinger <m.fellinger@gmail.com>
2008-12-30 18:00:30 +0900

    Refactor option.rb

--- lib/innate/option.rb
+++ lib/innate/option.rb
@@ -37,154 +37,86 @@
 #   * Find a faster way to access, this implementation is even slower than
 #     OpenStruct
 
-class Options < BasicObject
-  VERSION = '2008-09-05'
-  SCOPE = {}
+module Innate
+  class Options < BasicObject
+    VERSION = '2008-09-05'
+    SCOPE = {}
 
-  def self.for(name)
-    name = name.to_s
-    yield(SCOPE[name] ||= new(name)) if block_given?
-    SCOPE[name]
-  end
-
-  def self.each(name)
-    SCOPE[name.to_s].__hash.each do |key, value|
-      yield(key, value)
+    def self.for(name)
+      name = name.to_s
+      yield(SCOPE[name] ||= new(name)) if block_given?
+      SCOPE[name]
     end
-  end
 
-  attr_accessor :__name, :__hash
-
-  def initialize(name)
-    @__name = name
-    @__hash = {}
-  end
-
-  def [](key)
-    @__hash[key.to_s]
-  end
-
-  def []=(key, value)
-    @__hash[key.to_s] = value
-  end
-
-  # NOTE: 1.9 insists on '::Options', probably some kind of magic that would be
-  #       performed by const_missing doesn't apply for BasicObject
-  def method_missing(meth, *args, &block)
-    meth = meth.to_s
-
-    if meth =~ /:/
-      # can only be issued by __send__, but one cannot be careful enough
-      raise "':' is not allowed in a key"
-    elsif meth =~ /^(.+)=$/
-      SCOPE.delete("#@__name:#$1")
-      self[$1] = args.first
-    elsif block
-      self[meth] = ::Options.for("#@__name:#{meth}", &block)
-    else
-      splat = @__name.split(':')
-
-      splat.size.downto(1) do |n|
-        value = SCOPE[splat[0, n] * ':'][meth]
-
-        return value unless value.nil?
+    def self.each(name)
+      SCOPE[name.to_s].__hash.each do |key, value|
+        yield(key, value)
       end
-
-      return nil
     end
-  end
 
-  def to_hash
-    @__hash
-  end
+    attr_accessor :__name, :__hash
 
-  def pretty_print(q)
-    q.pp_hash @__hash
-  end
+    def initialize(name)
+      @__name = name
+      @__hash = {}
+    end
 
-  def inspect
-    @__hash.inspect
-  end
-end
+    def [](key)
+      @__hash[key.to_s]
+    end
 
-__END__
+    def []=(key, value)
+      @__hash[key.to_s] = value
+    end
 
-require 'bacon'
-Bacon.extend(Bacon::TestUnitOutput)
-Bacon.summary_on_exit
+    # NOTE: 1.9 insists on '::Innate::Options', probably some kind of magic that would be
+    #       performed by const_missing doesn't apply for BasicObject
+    def method_missing(meth, *args, &block)
+      meth = meth.to_s
 
-describe Options do
-  should 'create scope' do
-    Options.for :site do |site|
-      site.title = 'araiguma'
+      if meth =~ /:/
+        # can only be issued by __send__, but one cannot be careful enough
+        raise "':' is not allowed in a key"
+      elsif meth =~ /^(.+)=$/
+        SCOPE.delete("#@__name:#$1")
+        self[$1] = args.first
+      elsif block
+        self[meth] = ::Innate::Options.for("#@__name:#{meth}", &block)
+      else
+        splat = @__name.split(':')
 
-      site.admin do |admin|
-        admin.name = 'manveru'
-        admin.pass = 'letmein'
+        splat.size.downto(1) do |n|
+          key = splat[0, n] * ':'
+          value = SCOPE[key][meth]
+
+          unless value.nil?
+            code = "def %s; SCOPE[%p][%p]; end" % [meth, key, meth]
+            self.class.class_eval("def %s; SCOPE[%p][%p]; end" % [meth, key, meth])
+            return value
+          end
+        end
+
+        return nil
       end
-
-      site.db = 'sqlite'
     end
 
-    Options.for(:site).title.should == 'araiguma'
-  end
+    # BasicObject doesn't have #class
+    # This is a major problem when doing subclassing...
 
-  should 'inherit scope' do
-    Options.for(:site).admin.title.should == 'araiguma'
-  end
-
-  should 'access scope by for' do
-    Options.for(:site).admin.name.should == 'manveru'
-    Options.for('site:admin').name.should == 'manveru'
-  end
-
-  should 'replace scope with value' do
-    Options.for(:site).db = 1
-    Options.for(:site).db.should == 1
-    Options::SCOPE['site:db'].should == nil
-  end
-
-  should 'replace value with scope' do
-    Options.for(:site).db do |db|
-      db.logging = true
+    def class
+      Options
     end
 
-    Options.for(:site).db.logging.should == true
-  end
-
-  should 'iterate options' do
-    expect = %w[admin db title]
-
-    Options.each(:site) do |key, value|
-      expect.delete key
+    def to_hash
+      @__hash
     end
 
-    expect.should.be.empty
+    def pretty_print(q)
+      q.pp_hash @__hash
+    end
+
+    def inspect
+      @__hash.inspect
+    end
   end
-end
-
-__END__
-
-require 'ostruct'
-require 'benchmark'
-
-Options.for :a do |a|
-  a.x = 1
-end
-
-$os = OpenStruct.new(:x => 1)
-
-def bench_options
-  Options.for('a')['x']
-end
-
-def bench_ostruct
-  $os.x
-end
-
-Benchmark.bmbm(10) do |b|
-  n = 100_000
-  b.report('Options    :'){ n.times{ bench_options } }
-  b.report('OpenStruct :'){ n.times{ bench_ostruct } }
-  b.report('empty      :'){ n.times{ nil } }
 end
