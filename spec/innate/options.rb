4e5f3f25e711fe440aee7d83989502361f7e5635
Michael Fellinger <m.fellinger@gmail.com>
2009-01-05 18:57:48 +0900

    Update specs for new options

--- spec/innate/options.rb
+++ spec/innate/options.rb
@@ -1,5 +1,5 @@
 require 'lib/innate/core_compatibility/basic_object'
-require 'lib/innate/option'
+require 'lib/innate/options/dsl'
 
 require 'bacon'
 
@@ -9,51 +9,38 @@ Bacon.summary_on_exit
 Options = Innate::Options
 
 describe Options do
+  @options = Innate::Options.new(:spec)
+
+  should 'create option' do
+    @options.o('my name', :name, 'manveru')
+    @options.name.should == 'manveru'
+  end
+
+  should 'create options with meta hash' do
+    @options.o('the port', :port, 7000, :cli => '-p')
+    @options.port.should == 7000
+  end
+
+  should 'get complete hash via #get' do
+    @options.get(:port)[:cli].should == '-p'
+    @options.get(:port)[:doc].should == 'the port'
+  end
+
+  should 'get value via []' do
+    @options[:port].should == 7000
+  end
+
   should 'create scope' do
-    Options.for :site do |site|
-      site.title = 'araiguma'
-
-      site.admin do |admin|
-        admin.name = 'manveru'
-        admin.pass = 'letmein'
-      end
-
-      site.db = 'sqlite'
-    end
-
-    Options.for(:site).title.should == 'araiguma'
+    @options.sub(:deep)
+    @options.deep.should.not.be.nil
   end
 
-  should 'inherit scope' do
-    Options.for(:site).admin.title.should == 'araiguma'
+  should 'create option in scope' do
+    @options.deep.o('the browser', :browser, :firefox)
+    @options.deep.browser.should == :firefox
   end
 
-  should 'access scope by for' do
-    Options.for(:site).admin.name.should == 'manveru'
-    Options.for('site:admin').name.should == 'manveru'
-  end
-
-  should 'replace scope with value' do
-    Options.for(:site).db = 1
-    Options.for(:site).db.should == 1
-    Options::SCOPE['site:db'].should == nil
-  end
-
-  should 'replace value with scope' do
-    Options.for(:site).db do |db|
-      db.logging = true
-    end
-
-    Options.for(:site).db.logging.should == true
-  end
-
-  should 'iterate options' do
-    expect = %w[admin db title]
-
-    Options.each(:site) do |key, value|
-      expect.delete key
-    end
-
-    expect.should.be.empty
+  should 'search in higher scope if key not found' do
+    @options.deep.port.should == 7000
   end
 end
