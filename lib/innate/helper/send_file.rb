2aa69fc15a7f2e7803628e3ac7a2551a0d13d19c
Michael Fellinger <m.fellinger@gmail.com>
2009-01-17 14:00:40 +0900

    Port Helper::SendFile and spec

---
+++ lib/innate/helper/send_file.rb
@@ -0,0 +1,18 @@
+module Innate
+  module Helper
+    module SendFile
+      # Not optimally performing but convenient way to send files by their
+      # filename.
+      def send_file(filename, content_type = nil)
+        content_type ||= Rack::Mime.mime_type(::File.extname(filename))
+
+        response.body = ::File.readlines(filename, 'rb')
+        response['Content-Length'] = ::File.size(filename).to_s
+        response['Content-Type'] = content_type
+        response.status = 200
+
+        throw :respond
+      end
+    end
+  end
+end
