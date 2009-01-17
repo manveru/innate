8dfca77a9ba8010ab9943337fcd1ec7c1225835b
Michael Fellinger <m.fellinger@gmail.com>
2009-01-17 13:51:25 +0900

    Don't require rubygems twice on specs

--- lib/innate/spec.rb
+++ lib/innate/spec.rb
@@ -1,11 +1,9 @@
-begin; require 'rubygems'; rescue LoadError; end
+require File.expand_path(File.join(File.dirname(__FILE__), '../innate'))
 
 require 'bacon'
 
 Bacon.summary_on_exit
 Bacon.extend(Bacon::TestUnitOutput)
-
-require File.expand_path(File.join(File.dirname(__FILE__), '../innate'))
 
 Innate.middleware :innate do |m|
   m.use Innate::Current
