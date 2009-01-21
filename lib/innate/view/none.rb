c6e00160ec796580967a87ff306e18bd9a1b822c
Michael Fellinger <m.fellinger@gmail.com>
2009-01-21 13:10:10 +0900

    Simpler Innate::View::None

--- lib/innate/view/none.rb
+++ lib/innate/view/none.rb
@@ -1,9 +1,7 @@
 module Innate
   module View
     module None
-      module_function
-
-      def render(action, string = nil)
+      def self.render(action, string = nil)
         string || action.view
       end
     end
