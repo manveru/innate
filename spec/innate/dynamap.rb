27fa85af9b1c9c92b6560a7a694826b73a609494
Michael Fellinger <m.fellinger@gmail.com>
2009-01-24 00:52:02 +0900

    Add specs for DynaMap

---
+++ spec/innate/dynamap.rb
@@ -0,0 +1,22 @@
+require 'spec/helper'
+
+describe Innate::DynaMap do
+  @app = lambda{|env| [200, {}, ['pass']] }
+
+  should 'raise if nothing is mapped' do
+    lambda{ Innate::DynaMap.call({}) }.should.raise(RuntimeError)
+  end
+
+  should 'not raise if nothing is mapped' do
+    Innate.map('/', &@app)
+    Innate::DynaMap.call('SCRIPT_NAME' => '/').should == [200, {}, ['pass']]
+  end
+
+  should 'return mapped object' do
+    Innate.at('/').should == @app
+  end
+
+  should 'return path to object' do
+    Innate.to(@app).should == '/'
+  end
+end
