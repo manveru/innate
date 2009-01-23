8e1ee7d65249aa918ec27ea254573f112ddeffc3
Michael Fellinger <m.fellinger@gmail.com>
2009-01-24 00:51:46 +0900

    Spec more of Request

--- spec/innate/request.rb
+++ spec/innate/request.rb
@@ -5,6 +5,31 @@ describe Innate::Request do
     Innate::Request.new(env)
   end
 
+  @env = {
+    "GATEWAY_INTERFACE"    => "CGI/1.1",
+    "HTTP_ACCEPT"          => "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+    "HTTP_ACCEPT_CHARSET"  => "UTF-8,*",
+    "HTTP_ACCEPT_ENCODING" => "gzip,deflate",
+    "HTTP_ACCEPT_LANGUAGE" => "en-us,en;q=0.8,de-at;q=0.5,de;q=0.3",
+    "HTTP_CACHE_CONTROL"   => "max-age=0",
+    "HTTP_CONNECTION"      => "keep-alive",
+    "HTTP_HOST"            => "localhost:7000",
+    "HTTP_KEEP_ALIVE"      => "300",
+    "HTTP_USER_AGENT"      => "Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.5) Gecko/2008123017 Firefox/3.0.4 Ubiquity/0.1.4",
+    "HTTP_VERSION"         => "HTTP/1.1",
+    "PATH_INFO"            => "/",
+    "QUERY_STRING"         => "?a=b",
+    "REMOTE_ADDR"          => "127.0.0.1",
+    "REMOTE_HOST"          => "delta.local",
+    "REQUEST_METHOD"       => "GET",
+    "REQUEST_PATH"         => "/",
+    "REQUEST_URI"          => "http://localhost:7000/",
+    "SCRIPT_NAME"          => "",
+    "SERVER_NAME"          => "localhost",
+    "SERVER_PORT"          => "7000",
+    "SERVER_PROTOCOL"      => "HTTP/1.1",
+  }
+
   should 'provide #request_uri' do
     request('REQUEST_URI' => '/?a=b').request_uri.should == '/?a=b'
     request('PATH_INFO' => '/').request_uri.should == '/'
@@ -26,5 +51,47 @@ describe Innate::Request do
     req.params.should == params
     req.subset(:a).should == {'a' => 'b'}
     req.subset(:a, :c).should == {'a' => 'b', 'c' => 'd'}
+  end
+
+  should 'provide #domain' do
+    request(@env.merge('rack.url_scheme' => 'http')).domain.should ==
+      URI('http://localhost:7000/')
+    request(@env.merge('rack.url_scheme' => 'https')).domain.should ==
+      URI('https://localhost:7000/')
+    request(@env.merge('rack.url_scheme' => 'https')).domain('/foo').should ==
+      URI('https://localhost:7000/foo')
+  end
+
+  should 'provide #accept_language' do
+    request(@env).accept_language.should == %w[en-us en de-at de]
+  end
+
+  should 'provide #http_variables' do
+    r = request(@env).http_variables
+    r.should.not.be.empty
+  end
+
+  should 'provide #robust_params' do
+    params = {
+      'user[name]' => 'manveru',
+      'user[age]'  => '42',
+      'browser' => 'firefox',
+      'engine[webkit][version]' => '1.1',
+      'engine[webkit][browser]' => 'arora',
+      'engine[gecko][version]' => '1.2',
+      'engine[gecko][browser]' => 'firefox',
+    }
+    env = { 'rack.request.form_hash' => params }
+
+    request(env).robust_params.should == {
+      'user' => {'name' => 'manveru', 'age' => '42'},
+      'browser' => 'firefox',
+      'engine' => {
+        'webkit' => {
+          'version' => '1.1', 'browser' => 'arora'},
+        'gecko' => {
+          'version' => '1.2', 'browser' => 'firefox'}
+        }
+      }
   end
 end
