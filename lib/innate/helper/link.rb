dd778bad541980970b92e17cfcd9c67470470a5d
Michael Fellinger <m.fellinger@gmail.com>
2009-01-24 14:04:18 +0900

    Docs for Helper::Link#route

--- lib/innate/helper/link.rb
+++ lib/innate/helper/link.rb
@@ -5,6 +5,22 @@ module Innate
         into.extend(self)
       end
 
+      # Provide the path to given Node and actions.
+      # Escapes GET parameters.
+      #
+      # Usage, mapping is Pages => '/', Users => '/users':
+      #
+      #   Pages.r                       # => URI('/')
+      #   Pages.r(:foo)                 # => URI('/foo')
+      #   Pages.r(:foo, :bar)           # => URI('/foo/bar')
+      #   Pages.r(:foo, :a => :b)       # => URI('/foo?a=b')
+      #   Pages.r(:foo, :bar, :a => :b) # => URI('/foo/bar?a=b')
+      #
+      #   Users.r                       # => URI('/users/')
+      #   Users.r(:foo)                 # => URI('/users/foo')
+      #   Users.r(:foo, :bar)           # => URI('/users/foo/bar')
+      #   Users.r(:foo, :a => :b)       # => URI('/users/foo?a=b')
+      #   Users.r(:foo, :bar, :a => :b) # => URI('/users/foo/bar?a=b')
       def route(name = '/', *args)
         hash = {}
         hashes, names = args.partition{|arg| arg.respond_to?(:merge!) }
@@ -28,7 +44,6 @@ module Innate
       #   Wiki.a('home', :home)           # => '<a href="/wiki/home">home</a>'
       #   Wiki.a('home', :/)              # => '<a href="/wiki/">home</a>'
       #   Wiki.a('foo', :/, :foo => :bar) # => '<a href="/wiki/?foo=bar">foo</a>'
-
       def anchor(text, *args)
         href = args.empty? ? r(text) : r(*args)
         text = Rack::Utils.escape_html(text)
