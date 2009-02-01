20518d4b8722aded8e6cf8477b7910fbd8129229
Tadahiko Uehara <kikofx@gmail.com>
2009-02-02 03:37:47 +0900

    update aspect spec to check before/after_all, instance_eval and view without method.

--- spec/innate/helper/aspect.rb
+++ spec/innate/helper/aspect.rb
@@ -1,4 +1,6 @@
 require 'spec/helper'
+Innate.options.app.root = File.dirname(__FILE__)
+Innate.options.app.view = ''
 
 class AspectSpec
   include Innate::Node
@@ -13,6 +15,29 @@ class AspectSpec
 
   wrap(:with_wrap){ $aspect_spec_wrap += 20 }
   def with_wrap; $aspect_spec_wrap += 2; end
+  
+  before(:with_instance_var){ @foo = 'Hello'; @bar = 'World' }
+  def with_instance_var; "#{@foo} #{@bar}"; end
+end
+
+class AspectAllSpec
+  include Innate::Node
+  map '/all'
+  provide :html => :none
+
+  before_all{ $aspect_spec_before_all += 40; @foo = 'Hello'; @bar = 'World' }
+  after_all{  $aspect_spec_after_all += 40 }
+  def before_first; $aspect_spec_before_all +=2 ; end
+  def before_second; $aspect_spec_before_all +=2; end
+  def with_instance_var_first; "#{@foo} #{@bar}"; end
+  def with_instance_var_second; "#{@foo} to the #{@bar}"; end
+end
+
+class AspecNoMethodSpec
+  include Innate::Node
+  map '/without_method'
+  view_root 'view'
+  before_all{ @foo = 'Hello'; @bar = 'World'}
 end
 
 describe Innate::Helper::Aspect do
@@ -34,5 +59,22 @@ describe Innate::Helper::Aspect do
     $aspect_spec_wrap = 0
     get('/with_wrap').body.should == '22'
     $aspect_spec_wrap == 42
+  end
+
+  should 'before_all and after_all' do
+    $aspect_spec_before_all = $aspect_spec_after_all = 0
+    get('/all/before_first').body.should == '42'
+    $aspect_spec_before_all.should == 42
+    $aspect_spec_after_all.should == 40
+    get('/all/before_second').body.should == '84'
+    $aspect_spec_before_all.should == 84
+    $aspect_spec_after_all.should == 80
+  end
+
+  should 'instance variables in blocks available to view/method' do
+    get('/with_instance_var').body.should == 'Hello World'
+    get('/all/with_instance_var_first').body.should == 'Hello World'
+    get('/all/with_instance_var_second').body.should == 'Hello to the World'
+    get('/without_method/aspect_hello').body.should == "Hello World!\n"
   end
 end
