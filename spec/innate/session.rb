3d10ef2e46c502a6464a639302b1bf88b90a535a
Michael Fellinger <m.fellinger@gmail.com>
2009-02-19 14:40:13 +0900

    Clean up session spec

--- spec/innate/session.rb
+++ spec/innate/session.rb
@@ -1,9 +1,7 @@
 require 'spec/helper'
 
 class SessionSpec
-  include Innate::Node
-  map '/'
-  provide :html => :none
+  Innate.node('/').provide(:html => :none)
 
   def index
     'No session here'
@@ -30,29 +28,27 @@ class SessionSpec
   end
 end
 
-Innate.options.cache.default = Innate::Cache::Memory
-
-Innate.setup_dependencies
-
 describe 'Innate::Session' do
+  behaves_like :session
+
   should 'initiate session as needed' do
-    Innate::Mock.session do |session|
-      response = session.get('/')
+    session do |mock|
+      response = mock.get('/')
       response.body.should == 'No session here'
       response['Set-Cookie'].should == nil
 
-      session.get('/init').body.should == '0'
+      mock.get('/init').body.should == '0'
 
       1.upto(10) do |n|
-        session.get('/increment').body.should == n.to_s
+        mock.get('/increment').body.should == n.to_s
       end
 
-      session.get('/reset')
-      session.get('/view').body.should == ''
-      session.get('/init').body.should == '0'
+      mock.get('/reset')
+      mock.get('/view').body.should == ''
+      mock.get('/init').body.should == '0'
 
       -1.downto(-10) do |n|
-        session.get('/decrement').body.should == n.to_s
+        mock.get('/decrement').body.should == n.to_s
       end
     end
   end
