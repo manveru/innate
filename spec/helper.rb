1e09f3cd1d8126b40516371671cc69f8320cb14f
Michael Fellinger <m.fellinger@gmail.com>
2009-03-19 21:47:03 +0900

    Improve spec/helper.rb to work with rcov

--- spec/helper.rb
+++ spec/helper.rb
@@ -1,4 +1,10 @@
-$0 = caller.last[/^[^:]+/] unless caller.empty?
+if caller_line = caller.grep(%r!spec/innate/!).first
+  caller_file = caller_line.split(':', 2).first
+  caller_root = File.dirname(caller_file)
+  $0 = caller_file
+end
 
 require File.expand_path(File.join(File.dirname(__FILE__), '../lib/innate'))
 require 'innate/spec'
+
+Innate.options.roots = [caller_root]
