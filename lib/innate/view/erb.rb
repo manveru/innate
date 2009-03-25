63677932b5f2760a30fe02315ec85a0fa3c22770
Michael Fellinger <m.fellinger@gmail.com>
2009-03-25 18:01:07 +0900

    Remove cruft from View::ERB

--- lib/innate/view/erb.rb
+++ lib/innate/view/erb.rb
@@ -4,10 +4,6 @@ module Innate
   module View
     module ERB
       def self.call(action, string)
-        return unless string.respond_to?(:to_str)
-
-        action.copy_variables
-
         erb = ::ERB.new(string.to_str, nil, '%<>')
         erb.filename = (action.view || action.method).to_s
         html = erb.result(action.binding)
