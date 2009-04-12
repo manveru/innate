89204827a4b956bd1c5712059c54e0e6113b4505
Michael Fellinger <m.fellinger@gmail.com>
2009-04-12 14:03:21 +0900

    Improvment and spec for provide example

--- example/provides.rb
+++ example/provides.rb
@@ -14,15 +14,17 @@ class BlogArticles
   Innate.node('/')
 
   # provide a content representation for requests to /<action>.yaml
+  # If you request `/list.yaml`, you will get the `ARTICLES object serialized
+  # to YAML.
   provide(:yaml, :type => 'text/yaml'){|action, value| value.to_yaml }
 
-  # the return value of this method is the `value` in the provide above
-  # Since ruby has implicit returns, `ARTICLES` will be the return value.
-  #
-  # If you request `/list.yaml` you will get the `ARTICLES` object serialized
-  # to YAML.
+  # Since there will always be an `html` representation (the default), you have
+  # to take care of it. If you simply want to return an empty page, use following.
+  provide(:html){|action, value| '' }
+
+  # The return value of this method is the `value` in the provides above.
   def list
-    @articles = ARTICLES
+    return ARTICLES
   end
 end
 
---
+++ spec/example/provides.rb
@@ -0,0 +1,16 @@
+require 'spec/helper'
+require 'example/provides'
+
+describe 'examples/provide' do
+  behaves_like :mock
+
+  it 'provides YAML representation' do
+    get '/list.yaml'
+    last_response.body.should == ARTICLES.to_yaml
+    last_response['Content-Type'].should == 'text/yaml'
+
+    get '/list'
+    last_response.body.should == ''
+    last_response['Content-Type'].should == 'text/html'
+  end
+end
