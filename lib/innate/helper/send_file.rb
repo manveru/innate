15078e64cb26aa44106ae60c540132a3b243368a
Michael Fellinger <m.fellinger@gmail.com>
2009-04-16 01:49:04 +0900

    Add comment about planned deprecation of SendFile helper's default status

--- lib/innate/helper/send_file.rb
+++ lib/innate/helper/send_file.rb
@@ -3,6 +3,12 @@ module Innate
     module SendFile
       # Not optimally performing but convenient way to send files by their
       # filename.
+      #
+      # I think we should remove this from the default helpers and move it into
+      # Ramaze, the functionality is almost never used, the naming is ambigous,
+      # and it doesn't use the send_file capabilities of frontend servers.
+      #
+      # So for now, I'll mark it for deprecation
       def send_file(filename, content_type = nil)
         content_type ||= Rack::Mime.mime_type(::File.extname(filename))
 
