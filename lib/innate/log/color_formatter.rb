cca6d4da1f1b29e43639e2ba27a0ce4d7ce2a7af
Michael Fellinger <m.fellinger@gmail.com>
2009-04-29 23:34:45 +0900

    Improve the ColorFormatter, output background as well

--- lib/innate/log/color_formatter.rb
+++ lib/innate/log/color_formatter.rb
@@ -1,19 +1,25 @@
 class Logger
   # Extended Formatter that supports ANSI colors.
+  #
+  # The basic mapping of ANSI colors is as follows:
+  #
+  #     | reset | bold | dark | underline | blink | negative
+  # MOD |     0 |    1 |    2 |         4 |     5 |        7
+  #
+  #    | black | red | green | yellow | blue | magenta | cyan | white
+  # FG |    30 |  31 |    32 |     33 |   34 |      35 |   36 |    37
+  # BG |    40 |  41 |    42 |     43 |   44 |      45 |   46 |    47
+  #
+  # The output is done by: "\e[#{mod};#{fg};#{bg}m#{string}\e[0m"
+  # The suffix is to reset the terminal to the original state again.
   class ColorFormatter < Formatter
     LEVEL_COLOR = {
-      'DEBUG'   => :blue,
-      'INFO'    => :white,
-      'WARN'    => :yellow,
-      'ERROR'   => :red,
-      'FATAL'   => :red,
-      'UNKNOWN' => :green,
-    }
-
-    COLOR_CODE = {
-      :reset => 0, :bold => 1, :dark => 2, :underline => 4, :blink => 5,
-      :negative => 7, :black => 30, :red => 31, :green => 32, :yellow => 33,
-      :blue => 34, :magenta => 35, :cyan => 36, :white => 37,
+      'DEBUG'   => "\e[0;34;40m%s\e[0m", # blue on black
+      'INFO'    => "\e[0;37;40m%s\e[0m", # white on black
+      'WARN'    => "\e[0;33;40m%s\e[0m", # yellow on black
+      'ERROR'   => "\e[0;31;40m%s\e[0m", # red on black
+      'FATAL'   => "\e[0;35;40m%s\e[0m", # red on black
+      'UNKNOWN' => "\e[0;32;40m%s\e[0m", # green on black
     }
 
     FORMAT_TIME = "%Y-%m-%d %H:%M:%S"
@@ -33,7 +39,7 @@ class Logger
     end
 
     def colorize(string, severity)
-      "\e[#{COLOR_CODE[LEVEL_COLOR[severity]]}m#{string}\e[0m"
+      LEVEL_COLOR[severity] % string
     end
 
     def self.color?(logdev)
