21bbd9b0f50a0943465f1ffba1029d70fef05c7a
Michael Fellinger <m.fellinger@gmail.com>
2009-05-05 15:08:06 +0900

    Remove some (c)

--- spec/innate/action/layout.rb
+++ spec/innate/action/layout.rb
@@ -1,6 +1,3 @@
-#          Copyright (c) 2009 Michael Fellinger m.fellinger@gmail.com
-# All files in this distribution are subject to the terms of the Ruby license.
-
 require 'spec/helper'
 
 class SpecActionLayout
