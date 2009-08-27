b69e9dd6882e1781169b40a120b4a85ca4259eaf
Michael Fellinger <m.fellinger@gmail.com>
2009-08-27 12:55:08 +0200

    Fix Options#pretty_print spec

--- spec/innate/options.rb
+++ spec/innate/options.rb
@@ -104,9 +104,7 @@ describe Options do
 
   should 'pretty_print' do
     require 'pp'
-    p = PP.new
-    @options.pretty_print(p)
-    lines = p.output.split(/\n/)
+    lines = @options.pretty_inspect.split(/\n/)
     lines.find_all{|l|
       /:doc/ === l &&
       /:value/ === l
