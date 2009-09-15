a908b327452e56f93f7d9aff5ad8fb52d0ae034f
Michael Fellinger <m.fellinger@gmail.com>
2009-09-15 16:15:48 +0200

    Fix typo

--- spec/innate/dynamap.rb
+++ spec/innate/dynamap.rb
@@ -7,7 +7,7 @@ describe Innate::DynaMap do
     lambda{ Innate::DynaMap.call({}) }.should.raise(RuntimeError)
   end
 
-  should 'not raise if nothing is mapped' do
+  should 'not raise if something is mapped' do
     Innate.map('/', &@app)
     Innate::DynaMap.call('SCRIPT_NAME' => '/').should == [200, {}, ['pass']]
   end
