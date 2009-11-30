1b736b793e9f31cf79fad2820f3f20a00802c2c8
Zoxc <zoxc32@gmail.com>
2009-12-01 04:17:23 +0800

    Major performance improvement for Etanni.

--- lib/innate/view/etanni.rb
+++ lib/innate/view/etanni.rb
@@ -1,34 +1,19 @@
 module Innate
   module View
     module Etanni
+      SEPARATOR = "E69t116A65n110N78i105S83e101P80a97R82a97T84o111R82"
+      START = "\n_out_ << <<#{SEPARATOR}.chomp!\n"
+      STOP = "\n#{SEPARATOR}\n"
+      REPLACEMENT = "#{STOP}\\1#{START}"
+
       def self.call(action, string)
-        etanni = View.compile(string){|str| Innate::Etanni.new(str) }
-        html = etanni.result(action.binding, (action.view || action.method))
+        etanni = View.compile(string) do |str|
+          temp = str.strip.gsub!(/<\?r\s+(.*?)\s+\?>/m, REPLACEMENT)
+          eval "Proc.new do _out_ = [<<#{SEPARATOR}.chomp!]\n#{temp}#{STOP}_out_.join end", nil, (action.view || action.method)
+        end
+        html = action.instance_eval(&etanni)
         return html, 'text/html'
       end
     end
   end
-
-  class Etanni
-    SEPARATOR = "E69t116A65n110N78i105S83e101P80a97R82a97T84o111R82"
-    START = "\n_out_ << <<#{SEPARATOR}.chomp!\n"
-    STOP = "\n#{SEPARATOR}\n"
-    REPLACEMENT = "#{STOP}\\1#{START}"
-
-    def initialize(template)
-      @template = template
-      compile
-    end
-
-    def compile
-      temp = @template.dup
-      temp.strip!
-      temp.gsub!(/<\?r\s+(.*?)\s+\?>/m, REPLACEMENT)
-      @compiled = "_out_ = [<<#{SEPARATOR}.chomp!]\n#{temp}#{STOP}_out_"
-    end
-
-    def result(binding, filename = '<Etanni>')
-      eval(@compiled, binding, filename).join
-    end
- end
 end
