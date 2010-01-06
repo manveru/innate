59f03da4856ee52863a92cc4dbb5e8a4ad6f0a1a
Michael Fellinger <m.fellinger@gmail.com>
2010-01-06 16:10:22 +0900

    Fix request spec for new rack behaviour

--- spec/innate/request.rb
+++ spec/innate/request.rb
@@ -48,7 +48,10 @@ describe Innate::Request do
 
     query = {'a' => 'b', 'c' => 'd', 'e' => 'f'}
     params = StringIO.new(Rack::Utils.build_query(query))
-    req = request('rack.request.form_hash' => params, 'rack.input' => params)
+    req = request(
+      'QUERY_STRING' => params.string,
+      'rack.input' => params
+    )
 
     req.params.should == query
     req.subset(:a).should == {'a' => 'b'}
