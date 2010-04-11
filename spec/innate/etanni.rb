32886da3c3970ff20e61f0c795ba300246bb08ef
Zoxc <zoxc32@gmail.com>
2010-04-11 13:42:13 +0800

    Updated Etanni specs.

--- spec/innate/etanni.rb
+++ spec/innate/etanni.rb
@@ -4,24 +4,24 @@ require 'innate/view/etanni'
 describe Etanni = Innate::Etanni do
   it 'renders a simple template' do
     etanni = Etanni.new('Hello, World!')
-    etanni.result(binding).should == 'Hello, World!'
+    etanni.result(self).should == 'Hello, World!'
   end
 
   it 'allows use of Etanni::SEPARATOR inside interplated code' do
     etanni = Etanni.new('Hello #{@name}')
     @name = "\n#{Etanni::SEPARATOR}\n"
-    etanni.result(binding).should == "Hello #{@name}"
+    etanni.result(self).should == "Hello #{@name}"
   end
 
   it 'fails with SyntaxError or NameError when the SEPARATOR is used inside a template' do
     lambda{
       etanni = Etanni.new("Hello \n#{Etanni::SEPARATOR}\n")
-      etanni.result(binding)
+      etanni.result(self)
     }.should.raise NameError
 
     lambda{
       etanni = Etanni.new("<h1>Hello \n#{Etanni::SEPARATOR}\n</h1>")
-      etanni.result(binding)
+      etanni.result(self)
     }.should.raise SyntaxError
   end
 end
